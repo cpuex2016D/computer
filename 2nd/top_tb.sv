654408471a41a271548b0de1697a1babec5d3cd7
sueki743 <sueki743@users.noreply.github.com>
2017-01-31 12:53:59 +0900

    [2nd] テストベンチ

--- 2nd/top_tb.sv
+++ 2nd/top_tb.sv
@@ -10,19 +10,22 @@ module top_tb;
 	logic[7:0] LED;
 
 	localparam PERIOD = 4;
+//	localparam PERIOD = 1292;
 	top #(
 		.RECEIVER_PERIOD(PERIOD/2),
 		.SENDER_PERIOD(PERIOD)
 	) top(.*);
-	localparam OP_ADD     = 6'b000000;
-	localparam OP_ADDI    = 6'b000001;
-	localparam OP_SUB     = 6'b000010;
-	localparam OP_SUBI    = 6'b000011;
-	localparam OP_SL2ADD  = 6'b000100;
-	localparam OP_SL2ADDI = 6'b000101;
-	localparam OP_IN      = 6'b011010;
-	localparam OP_OUT     = 6'b011100;
-	localparam OP_J       = 6'b100000;
+
+	logic sim_receiver_valid;
+	logic[7:0] sim_receiver_out;
+	receiver #(
+		.RECEIVER_PERIOD(PERIOD/2)
+	) sim_receiver(
+		.clk(top.clk),
+		.in(UART_TX),
+		.out(sim_receiver_out),
+		.valid(sim_receiver_valid)
+	);
 
 //	always #(0.5) begin
 //		CLK_P <= !CLK_P;
@@ -32,13 +35,28 @@ module top_tb;
 		force top.clk = !top.clk;
 	end
 
-	int fd;
+	int fd, fd_w;
 	int c;
+	int count = 0;
+
+	always @(posedge top.clk) begin
+		if (sim_receiver_valid) begin
+			$fwrite(fd_w, "%c", sim_receiver_out);
+			$fflush(fd_w);
+			count = count + 1;
+			if (count==160000) $finish;
+		end
+	end
 
 	initial begin
+		fd_w = $fopen("../../../o_sim", "wb");
+//		fd_w = $fopen("../../../../o_sim", "wb");
+
 		top.pc <= 0;
+		top.inst_mem.inst_mem <= '{default: 0};
 
 		fd = $fopen("../../../program_fib_text", "rb");
+//		fd = $fopen("../../../../program_init_text", "rb");
 		#10;
 		forever begin
 			c = $fgetc(fd);
@@ -59,7 +77,8 @@ module top_tb;
 		#10;
 		SW_E <= !SW_E;
 
-//		fd = $fopen("../../../program_loopback_data", "rb");
+//		fd = $fopen("../../../program_mandelbrot_data", "rb");
+////		fd = $fopen("../../../../program_mandelbrot_data", "rb");
 //		#10;
 //		forever begin
 //			c = $fgetc(fd);
@@ -74,5 +93,32 @@ module top_tb;
 //			#PERIOD;
 //			#1;
 //		end
+//
+//		#10;
+//		SW_W <= !SW_W;
+//		#10;
+//		SW_W <= !SW_W;
+//
+//		fd = $fopen("../../../program_mandelbrot_text", "rb");
+////		fd = $fopen("../../../../program_mandelbrot_text", "rb");
+//		#10;
+//		forever begin
+//			c = $fgetc(fd);
+//			if (c==-1) break;
+//			UART_RX <= 0;
+//			#PERIOD;
+//			for (int i=0; i<8; i++) begin
+//				UART_RX <= c[i];
+//				#PERIOD;
+//			end
+//			UART_RX <= 1;
+//			#PERIOD;
+//			#1;
+//		end
+//
+//		#10;
+//		SW_E <= !SW_E;
+//		#10;
+//		SW_E <= !SW_E;
 	end
 endmodule
