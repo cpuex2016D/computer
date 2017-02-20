bc4bdeb2b03e7dd39edac961346f226acb5bbecc
sueki743 <sueki743@users.noreply.github.com>
2017-02-20 17:18:27 +0900

    親コアと子コアのコードを変える

--- 2nd/inst_mem.sv
+++ 2nd/inst_mem.sv
@@ -23,8 +23,13 @@ module inst_mem #(
 	(* ram_style = "distributed" *) logic[1:0] pht[2**PATTERN_WIDTH];
 	logic[GH_WIDTH-1:0] gh = 0;
 	initial begin
-		$readmemh("text.hex", inst_mem);
-		inst.bits <= PARENT ? {4'h8, 4'b0, PC_INIT[13:11], 10'b0, PC_INIT[10:0]} : 32'h7c000000;
+		if (PARENT) begin
+			$readmemh("parent.text.hex", inst_mem);
+			inst.bits <= {4'h8, 4'b0, PC_INIT[13:11], 10'b0, PC_INIT[10:0]};
+		end else begin
+			$readmemh("child.text.hex", inst_mem);
+			inst.bits <= 32'h7c000000;
+		end
 	end
 
 	logic[INST_MEM_WIDTH-1:0] inst_addr;
